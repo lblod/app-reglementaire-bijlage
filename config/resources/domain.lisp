562112d788822b1aeb8258bf7d5894ce9600e6fe
Oscar <lagartoverde97@gmail.com>
2022-05-18 11:25:36 +0200

    move all the resources to json

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,4 +1,7 @@
 (in-package :mu-cl-resources)
 
 ;; reading in the domain.json
+(read-domain-file "domain.json")
 (read-domain-file "auth.json")
+(read-domain-file "editor.json")
+(read-domain-file "goverment.json")

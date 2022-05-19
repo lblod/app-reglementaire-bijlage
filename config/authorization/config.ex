6e16f4498beae6644378e71b68018a8b3e99f6ea
Oscar <lagartoverde97@gmail.com>
2022-05-19 12:44:03 +0200

    required role should be beheerder now

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -44,7 +44,7 @@ defmodule Acl.UserGroups.Config do
                   PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
                   SELECT DISTINCT ?session_group WHERE {
                     <SESSION_ID> ext:sessionGroup/mu:uuid ?session_group;
-                                 ext:sessionRole \"GelinktNotuleren-schrijver\".
+                                 ext:sessionRole \"beheerder\".
                     }" },
         graphs: [ %GraphSpec{
                     graph: "http://mu.semte.ch/graphs/organizations/",

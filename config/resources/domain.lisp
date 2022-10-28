e70340ce3cc7b43a0ec6fd8a8048b8f7925e5a41
Elena Poelman <elena.poelman@redpencil.io>
2022-10-28 13:14:50 +0200

    ensure count is include in meta object in paginated responses

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,5 +1,9 @@
 (in-package :mu-cl-resources)
 
+(defparameter *include-count-in-paginated-responses* t
+  "when non-nil, all paginated listings will contain the number
+   of responses in the result object's meta.")
+
 ;; reading in the domain.json
 (read-domain-file "domain.json")
 (read-domain-file "auth.json")

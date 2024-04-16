e4fbeef2cf7800c7d09c97f060e5a9f7bb391641
Elena Poelman <elena.poelman@redpencil.io>
2024-04-16 14:05:44 +0200

    restructure dispatcher configuration

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -16,72 +16,9 @@ defmodule Dispatcher do
   # Run `docker-compose restart dispatcher` after updating
   # this file.
 
-  ###############
-  # SPARQL
-  ###############
-
- match "/sparql", %{ layer: :api, accept: %{ sparql: true } } do
-    forward conn, [], "http://sparql-cache/sparql"
-  end
-
- match "/raw-sparql", %{ layer: :api, accept: %{ sparql: true } } do
-   forward conn, [], "http://database:8890/sparql"
- end
-
-   ###############################################################
-  # domain.json
-  ###############################################################
-
-  match "/regulatory-statements/*path" do
-    forward conn, path, "http://cache/regulatory-statements/"
-  end
-
-  match "/editor-documents/*path" do
-    forward conn, path, "http://cache/editor-documents/"
-  end
-
-  match "/editor-document-folders/*path" do
-    forward conn, path, "http://cache/editor-document-folders/"
-  end
-
-  match "/document-containers/*path" do
-    forward conn, path, "http://cache/document-containers/"
-  end
-
-  match "/administrative-units/*path" do
-    forward conn, path, "http://cache/administrative-units/"
-  end
-
-  #########
-  # login
-  ########
-  match "/mock/sessions/*path" do
-    forward conn, path, "http://mocklogin/sessions/"
-  end
-
-  match "/sessions/*path" do
-    forward conn, path, "http://login/sessions/"
-  end
-
-  match "/users/*path" do
-    forward conn, path, "http://cache/users/"
-  end
-
-  match "/accounts/*path" do
-    forward conn, path, "http://cache/accounts/"
-  end
-
-  match "/groups/*path" do
-    forward conn, path, "http://cache/groups/"
-  end
-
-  post "/remote-login/*path" do
-    forward conn, [], "http://remotelogin/remote-login"
-  end
-
-  ############
-  # resources
-  ############
+  #######################################################################
+  # domain.json                                                         #
+  #######################################################################
 
   match "/code-lists/*path", %{ accept: %{json: true}, layer: :api} do
     Proxy.forward conn, path, "http://cache/code-lists/"
@@ -131,9 +68,29 @@ defmodule Dispatcher do
     forward conn, path, "http://codex-proxy/sparql/"
   end
 
-  #########
-  # files
-  ########
+  #######################################################################
+  # editor.json                                                         #
+  #######################################################################
+
+  match "/editor-documents/*path" do
+    forward conn, path, "http://cache/editor-documents/"
+  end
+
+  match "/editor-document-folders/*path" do
+    forward conn, path, "http://cache/editor-document-folders/"
+  end
+
+  match "/document-containers/*path" do
+    forward conn, path, "http://cache/document-containers/"
+  end
+
+  match "/administrative-units/*path" do
+    forward conn, path, "http://cache/administrative-units/"
+  end
+
+  #######################################################################
+  # FILES                                                               #
+  #######################################################################
 
   get "/files/:id/download", %{ layer: :api} do
     Proxy.forward conn, [], "http://file/files/" <> id <> "/download"
@@ -151,10 +108,50 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://file/files/"
   end
 
+  #######################################################################
+  # SPARQL                                                              #
+  #######################################################################
 
-  ###############################################################
-  # frontend layer
-  ###############################################################
+  match "/sparql", %{ layer: :api, accept: %{ sparql: true } } do
+    forward conn, [], "http://sparql-cache/sparql"
+  end
+
+  match "/raw-sparql", %{ layer: :api, accept: %{ sparql: true } } do
+    forward conn, [], "http://database:8890/sparql"
+  end
+
+  #######################################################################
+  # AUTH                                                                #
+  #######################################################################
+
+  match "/mock/sessions/*path" do
+    forward conn, path, "http://mocklogin/sessions/"
+  end
+
+  match "/sessions/*path" do
+    forward conn, path, "http://login/sessions/"
+  end
+
+  match "/users/*path" do
+    forward conn, path, "http://cache/users/"
+  end
+
+  match "/accounts/*path" do
+    forward conn, path, "http://cache/accounts/"
+  end
+
+  match "/groups/*path" do
+    forward conn, path, "http://cache/groups/"
+  end
+
+  post "/remote-login/*path" do
+    forward conn, [], "http://remotelogin/remote-login"
+  end
+
+
+  #######################################################################
+  # FRONTEND                                                            #
+  #######################################################################
 
   match "/assets/*path", %{ layer: :api } do
     Proxy.forward conn, path, "http://frontend/assets/"
